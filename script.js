5d665702efb4b28903c70f1a2fece875c6344739
Lizakiknadze <112972911+Lizakiknadze@users.noreply.github.com>
2022-11-15 19:51:48 +0400

    d

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c
 const main = document.getElementById('main')
 const form = document.getElementById('inputs')
 const search = document.getElementById('search')
+const vote_average = document.getElementById('IMDB')
 
 getMovies(API_URL);
 async function getMovies(url){
@@ -24,9 +25,35 @@ function showMovies (movie){
         <div class = 'movie-info'>
         <h3>${title}</h3>
         <p>${overview}</p>
-        <span>${vote_average}</span>
+        <span class="${changeColor(vote_average)}>${vote_average}</span>
         </div>
         `
         main.appendChild(movieEl)
-    });
+    }); 
 }
+function changeColor(vote_average){
+    if(vote_average =>8){
+        return 'green';
+    }
+    else if(vote_average => 5){
+        return 'yellow'
+    }
+    else if(vote_average < 5){
+        return 'red'
+    }
+    
+}
+
+// function f_color(){
+//     if (document.getElementById('vote_average').value > 7,99) {
+//     document.getElementById('vote_average').style.color = "Green";
+//     }
+//     else if (document.getElementById('vote_average').value >5.99 && < 7.99) {
+//     document.getElementById('vote_average').style.color = "Orange";
+//     }
+//     else if (document.getElementById('vote_average').value = '1-5') {
+//     document.getElementById('vote_average').style.color = "Red";
+//     }
+//     }
+//     f_color();
+    
